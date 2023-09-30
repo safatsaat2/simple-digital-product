ec2c08f475aa5a68be27a5baced5f7b018c1a38b
safatsaat2 <sakibsafat47@gmail.com>
2023-09-30 12:14:43 -0700

    success page implementation

--- src/components/Success.tsx
+++ src/components/Success.tsx
@@ -2,8 +2,9 @@ type Props = {}
 
 const Success = (props: Props) =>{
     return(
-        <div>
-            <h1>Success</h1>
+        <div className="border rounded px-6 py-8 flex-col flex mt-8 items-start h-min container">
+            <h1>Your Order confirmation</h1>
+            <p className="text-gray-900">Your will receive an email with your order shortly</p>
         </div>
     )
 }
