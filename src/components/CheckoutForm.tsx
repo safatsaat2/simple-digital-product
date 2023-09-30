2c275334b23eb627bbc767696f14650fb3936a9b
safatsaat2 <sakibsafat47@gmail.com>
2023-09-30 11:53:00 -0700

    strip chckoutform implementation

---
+++ src/components/CheckoutForm.tsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+
+import {
+    PaymentElement,
+    Elements,
+    useStripe,
+    useElements,
+  } from '@stripe/react-stripe-js';
+
+
+export const CheckoutForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+  
+    const [errorMessage, setErrorMessage] = useState<string | undefined>("");
+  
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+  
+      if (elements == null || stripe == null) {
+        return;
+      }
+  
+      // Trigger form validation and wallet collection
+      const {error: submitError} = await elements.submit();
+      if (submitError?.message) {
+        // Show error to your customer
+        setErrorMessage(submitError.message);
+        return;
+      }
+  
+      // Create the PaymentIntent and obtain clientSecret from your server endpoint
+      const res = await fetch('https://d78c5nr8x4.us.aircode.run/payment', {
+        method: 'POST',
+      });
+  
+      const {client_secret: clientSecret} = await res.json();
+  
+      const {error} = await stripe.confirmPayment({
+        //`Elements` instance that was used to create the Payment Element
+        elements,
+        clientSecret,
+        confirmParams: {
+          return_url: `${window.location.origin}/success`,
+        },
+      });
+  
+      if (error) {
+        // This point will only be reached if there is an immediate error when
+        // confirming the payment. Show error to your customer (for example, payment
+        // details incomplete)
+        setErrorMessage(error.message);
+      } else {
+        // Your customer will be redirected to your `return_url`. For some payment
+        // methods like iDEAL, your customer will be redirected to an intermediate
+        // site first to authorize the payment, then redirected to the `return_url`.
+      }
+    };
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <PaymentElement />
+        <button type="submit" disabled={!stripe || !elements}>
+          Pay
+        </button>
+        {/* Show error message to your customers */}
+        {errorMessage && <div>{errorMessage}</div>}
+      </form>
+    );
+  };
+  
--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,13 +1,34 @@
 import ProductCard from "../components/ProductCard"
 import { PRODUCT } from "../productInfo"
+import {loadStripe} from '@stripe/stripe-js';
+import {
+  PaymentElement,
+  Elements,
+  ElementsConsumer,
+} from '@stripe/react-stripe-js';
+import { CheckoutForm } from "../components/CheckoutForm";
 
 type Props ={}
+const options = {
+    mode: 'payment',
+    amount: 1099,
+    currency: 'usd',
+    // Fully customizable with appearance API.
+    appearance: {
+      /*...*/
+    },
+  };
 
 const Checkout = (props: Props) =>{
+    const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PK);
+
     return(
         <div className="flex flex-col container mt-8">
                 <h1 className="text-center mb-5">Checkout</h1>
                 <ProductCard {...PRODUCT} />
+                <Elements stripe={stripePromise} options={options}>
+                    <CheckoutForm/>
+                </Elements>
 
         </div>
     )
