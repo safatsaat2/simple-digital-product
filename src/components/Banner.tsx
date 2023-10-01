caa070e9490b64ef1360fc70d648a5df9b71c87b
safatsaat2 <sakibsafat47@gmail.com>
2023-10-01 03:31:45 -0700

    banner image change also feb icon and title hange

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -8,13 +8,19 @@ type Props = {
 const Banner = (props: Props) => {
   return (
     <div className="px-2 py-20 border-b">
-      <div className="container grid grid-cols-4 items-end">
-          <h1 className="col-span-3 text-[100px]">
+      <div className="container grid grid-cols-4">
+          <h1 className="col-span-3 text-[100px] z-10">
           {props.title}
           <br />
           <span className="text-indigo-600">{props.title2}</span>
           </h1>
-          <Link className="bg-indigo-600 ml-5 mt-2 text-white rounded-md p-2 text-center transition-all duration-500" to="#product">See Product</Link>
+          <div className="flex flex-col justify-between">
+            <div className="rounded-full w-[300px] h-[300px] -ml-10 -mt-10 z-0 shape">
+
+            </div>
+            <Link className="bg-indigo-600 ml-5 mt-2 text-white rounded-md p-2 text-center transition-all duration-500" to="#product">See Product</Link>
+          </div>
+          
       </div>
     </div>
   );
